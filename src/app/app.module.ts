36c3bd04a24d20d9d58903a3eaeec508f349341e
vamsi900 <vamsireddy900@gmail.com>
2019-09-05 15:34:25 +0530

    before final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatTableModule} from '@angular/material/table';
 import { AssetComponent } from './asset/asset.component';
 import {MatCheckboxModule} from '@angular/material/checkbox';
+
 import { 
          MatSidenavModule,
          MatToolbarModule,
@@ -21,6 +22,8 @@ import {MatTabsModule} from '@angular/material/tabs';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatSelectModule} from '@angular/material/select';
 import {ScrollingModule} from '@angular/cdk/scrolling';
+import {MatSortModule} from '@angular/material/sort';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -30,7 +33,7 @@ import {ScrollingModule} from '@angular/cdk/scrolling';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
+    BrowserAnimationsModule,MatSortModule,
     MatTableModule,MatCheckboxModule,ScrollingModule,
     MatSidenavModule,MatDividerModule,MatSelectModule,
     MatToolbarModule,
